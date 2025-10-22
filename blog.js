d03cd450622177fe9866651e142abb628b6bbbc5
Ana Clara <anacsds1@gmail.com>
2025-10-22 15:51:33 -0300

    Adicionando inicio do HTML, CSS, JS e imagens

---
+++ blog.js
@@ -0,0 +1,35 @@
+const sistBlog = [ 
+    { 
+    "BlogMural": { 
+        "titulo": "Blog ou Mural de histórias", 
+        "descricao": "Um espaço para compartilhar depoimentos, histórias e conteúdos inspiradores.", 
+    },
+},
+]
+    const sistPost = [ 
+        { 
+            "idPost": "p1", "Nome": "Digite aqui...", 
+            "NovaHistória": "Digite aqui...", }, 
+        ]
+    const sistemaFeedback = [ 
+            { "id": "Amei" , 
+                "icone": "❤"
+                }, 
+                { "id": "Legal", 
+                    "icone": " " 
+
+                }, 
+                { "id": "Gostei", 
+                    "icone": " " 
+
+                }, 
+            ]
+        const sistemaComentarios = [
+                    { "nomeUsuario": "Digite aqui...", 
+                        "texto": "Digite aqui..."
+                        },
+                    ]
+
+
+
+                
